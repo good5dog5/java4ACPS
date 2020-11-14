3c77259a542ecc75690e70002cff215b5e9ff924
jordan <jordan.huang@gapp.nthu.edu.tw>
2020-11-14 13:14:39 +0800

    Gimzo initial question.

---
+++ src/main/java/W3/OnlinePurchaseManager.java
@@ -0,0 +1,26 @@
+package W3;
+
+import java.util.ArrayList;
+
+public class OnlinePurchaseManager
+{
+    /** An ArrayList of purchased Gizmo objects, instantiated in the constructor. */
+    private ArrayList<Gizmo> purchases;
+    /** Returns the number of purchased Gizmo objects that are electronic and are
+     * manufactured by maker, as described in part (a).
+     */
+    public int countElectronicsByMaker(String maker)
+    { /* to be implemented in part (a) */
+        // TODO
+        return 0;
+    }
+    /** Returns true if any pair of adjacent purchased Gizmo objects are equivalent, and
+     * false otherwise, as described in part (b).
+     */
+    public boolean hasAdjacentEqualPair()
+    { /* to be implemented in part (b) */
+        // TODO
+        return false;
+    }
+    // There may be instance variables, constructors, and methods not shown.
+}
